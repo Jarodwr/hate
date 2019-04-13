38ecff116660fa0c896458ec7f5cf3188a829a62
Jarod Wright <wrjarod@gmail.com>
2019-04-13 13:06:58 +1000

    add love debugger back

---
+++ library/love-debugger/main.lua
@@ -0,0 +1,21 @@
+love.window.setMode(800, 600, {highdpi = true})
+
+dbg = require ((...):match("(.-)[^%/%.]+$") .. 'love-debugger')
+
+dbg.writeln("Type 'h <return>' to list the commands you can use.")
+
+function love.draw()
+	love.graphics.clear(128/255, 191/255, 242/255)
+	love.graphics.print("Click to trigger a breakpoint or press any key to crash!")
+end
+
+function love.mousepressed()
+	dbg.writeln("Hey look! A breakpoint!")
+	dbg()
+end
+
+function love.keypressed()
+	local a_string = "five"
+	local a_number = 5
+	local crash = a_string + a_number
+end
